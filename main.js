17f8cb13ed83fed6b1301aebb984bddff673647d
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2022-11-19 21:37:17 +0400

    Finish main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let totalRounds = 5
+let wins = 0
+
 let RPS = ["Rock", "Paper", "Scissors"]
 let RPSR = {"Paper" : 1, "Scissors" : 2, "Rock" : 3}
 let getComputerChoice = () => {return RPS[Math.floor(Math.random() * RPS.length)]}
@@ -10,4 +13,23 @@ function playRound(playerSelection, computerSelection) {
         :  (playerSelection == "Rock" && computerSelection == "Paper") ? `You Lose! ${computerSelection} beats ${playerSelection}`
         :  (RPSR[playerSelection] > RPSR[computerSelection]) ? `You Win! ${playerSelection} beats ${computerSelection}` : `You Lose! ${computerSelection} beats ${playerSelection}`
 }
-console.log(playRound("Rock", getComputerChoice()))
+
+function game() {
+    wins = 0
+    for (let i = 0; i <= totalRounds; i++) {
+        let playerChoice = prompt("Type scissors, paper or rock!")
+        let result = playRound(playerChoice, getComputerChoice())
+        console.log(result)
+        wins += (result.includes("Win")) ? 1 : 0
+        if (totalRounds == i) {
+            if (wins > totalRounds/2) {
+                console.log(`You have won the game with ${wins} Wins!`)
+            } else if (wins < totalRounds/2) {
+                console.log(`You have lost the game with ${wins} Wins!`)
+            } else {
+                console.log("Game ended with a tie!")
+            }
+        }
+    }
+};
+game()
